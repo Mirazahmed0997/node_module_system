2be7925a8f9fd73984ff084148235865914a25da
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-08-23 02:33:49 +0600

    setHeaderResponse

--- ToDo_App/Server.js
+++ ToDo_App/Server.js
@@ -1,6 +1,27 @@
 const http= require("http")
+const { json } = require("stream/consumers")
 
 const PORT=5000
+
+const data= [
+  {
+    "title": "First Post",
+    "body": "This is the body of the first post.",
+    "created_at": "2025-08-23T02:30:00Z"
+  },
+  {
+    "title": "Second Post",
+    "body": "Here is some more content in the second post.",
+    "created_at": "2025-08-22T18:45:00Z"
+  },
+  {
+    "title": "Third Post",
+    "body": "Another entry with details about a new update.",
+    "created_at": "2025-08-21T12:10:00Z"
+  }
+]
+
+
 
 const server= http.createServer((req,res)=>
 {
@@ -8,7 +29,16 @@ const server= http.createServer((req,res)=>
     // res.end(" Welcome to Node server")
     if(req.url==='/todos' && req.method==='GET')
     {
-        res.end('Get All Todos')
+        res.writeHead(200,{
+            "content-type": "application/json",
+        })
+
+        // another way to set header
+
+        // res.setHeader("content-type", "text/plain")
+        // res.setHeader("email", "istypartho@gmail.com")
+        // res.statusCode=201
+        res.end(JSON.stringify(data))
     }
     else if(req.url==='/todos/create_todos' && req.method==='POST'){
         res.end('Create Todos')
