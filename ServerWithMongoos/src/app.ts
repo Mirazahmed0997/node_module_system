b8e75bbcf65113c255a4b8c796c3a8f566ccafb6
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-09-05 00:46:53 +0600

    schema&model

--- ServerWithMongoos/src/app.ts
+++ ServerWithMongoos/src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, request, Request, response, Response } from 'express';
 import fs from "fs"
+import mongoose, { model, Schema } from 'mongoose';
 import path from "path"
+import { title } from 'process';
 
 
 
@@ -8,7 +10,30 @@ const app: Application = express();
 app.use(express.json())
 const userRouter=express.Router();
 
+const noteSchema= new Schema({
+  title:String,
+  content: String
+})
 
+
+const Note = mongoose.model("Note", noteSchema)
+
+app.post('/create-note',async(req: Request,res:Response)=>
+{
+  const myNote= new Note({
+    title: "Database",
+    content: "Learnig Mongoose",
+   
+  })
+
+  await myNote.save()
+
+  res.status(201).json({
+    success: true,
+    message: "Note created Successfully Created",
+    note: myNote
+  })
+})
 
 app.get('/', (req: Request, res: Response) => {
   
--- ServerWithMongoos/src/server.ts
+++ ServerWithMongoos/src/server.ts
@@ -10,7 +10,7 @@ const port=5000;
 async function main()
 {
   try {
-     await mongoose.connect('mongodb+srv://mongoDB:mongoDB@cluster0.ljhdru4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+     await mongoose.connect('mongodb+srv://mongoDB:mongoDB@cluster0.ljhdru4.mongodb.net/Note-App?retryWrites=true&w=majority&appName=Cluster0');
      console.log("connected to mongoose")
      app.listen(port, async () => {
      console.log(`🚀 Server is running at http://localhost:${port}`);
